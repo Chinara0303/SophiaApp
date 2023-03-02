943439703920d692ba7491843cc032c14f99f1d5
Chinara0303 <chinaraei@code.edu.az>
2023-03-02 20:42:18 +0400

    FEAT: Added intro,info sections

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     //currency
-    $(document).on("click",".title-currency", function () {
+    $(document).on("click", ".title-currency", function () {
         $(".currency").toggleClass("d-none");
         // $(".languages").toggleClass("d-none");
     })
@@ -17,36 +17,49 @@ $(document).ready(function () {
     })
 
     //language
-    $(document).on("click",".title-lang",function () {
+    $(document).on("click", ".title-lang", function () {
         $(".languages").toggleClass("d-none");
         // $(".currency").toggleClass("d-none");
     })
+    $(document).on("click", "main", function () {
+        if (!$(".sub-menu").hasClass("d-none")) {
+            $(".sub-menu").addClass("d-none")
+        }
+        // $(".currency").toggleClass("d-none");
+    })
 
-    $(document).on("click",".item",function(){
-       let clickedFLag = $(this).children().eq(0).attr("src");
-       let clickedText = $(this).children().eq(1).text();
-       $(this).parent().prev().children().eq(0).attr("src",clickedFLag);
-       $(this).parent().prev().children().eq(1).text(clickedText);
-       $(".languages").toggleClass("d-none");
+    $(document).on("click", ".item", function () {
+        let clickedFLag = $(this).children().eq(0).attr("src");
+        let clickedText = $(this).children().eq(1).text();
+        $(this).parent().prev().children().eq(0).attr("src", clickedFLag);
+        $(this).parent().prev().children().eq(1).text(clickedText);
+        $(".languages").toggleClass("d-none");
     })
 
     //submenu
-    $(document).on("click",".last-menu",function(e){
+    $(document).on("click", ".last-menu", function (e) {
         e.preventDefault();
         $(".sub-menu").toggleClass("d-none")
     })
 
     //phone-menu
-    $(document).on("click",".open-icon",function(){
+    $(document).on("click", ".open-icon", function () {
         $(".menu").addClass("active-menu");
-        $(".overlay").css("display","block")
+        $(".overlay").css("display", "block")
     })
-    $(document).on("click",".close-icon",function(){
+    $(document).on("click", ".close-icon", function () {
         $(".menu").removeClass("active-menu");
-        $(".overlay").css("display","none");
+        $(".overlay").css("display", "none");
     })
-    $(document).on("click",".overlay",function(){
+    $(document).on("click", ".overlay", function () {
         $(".menu").removeClass("active-menu");
-        $(".overlay").css("display","none");
+        $(".overlay").css("display", "none");
     })
-})
+    //slider
+
+    $('.slider-intro').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+});
