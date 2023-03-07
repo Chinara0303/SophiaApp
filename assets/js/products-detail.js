ad4999581637b09b69a83716cf50458ed922d2bf
Chinara0303 <chinaraei@code.edu.az>
2023-03-07 15:42:29 +0400

    REFACTOR: Product detail

--- assets/js/products-detail.js
+++ assets/js/products-detail.js
@@ -7,9 +7,11 @@ $(document).ready(function () {
     if (productDetailsInfo != null) {
         for (const product of productDetailsInfo) {
             $("#detail-intro .left-side .img img").attr("src", product.img)
+            $("#detail-intro .left-side .slider-area .box").children().eq(0).attr("src", product.img)
             $("#detail-intro .left-side  .information h2").text(product.name)
             $("#detail-intro .left-side  .information .prices del").text(`$${product.nativePrice}`)
             $("#detail-intro .left-side  .information .prices .native-price span").text(`$${product.discountPrice}`)
+            $(".prod-name").text(`${product.name}`)
         }
     }
     basket = JSON.parse(localStorage.getItem("basket"));
@@ -36,4 +38,36 @@ $(document).ready(function () {
             $(".zoom-modal-area").children().eq(0).attr("src", product.img)
         }
     })
+    
+    $('.slider-area').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 })
